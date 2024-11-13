9d80a5579ead13a93b9ca99e169fd6a32b98523b
MajidKh230 <khanmajid230@gmail.com>
2024-11-13 19:21:03 +0500

    add persist

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import { router } from './Routes/routes.jsx'
-import { Provider } from 'react-redux'
-import { store } from './store/store.js'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./Routes/routes.jsx";
+import { Provider } from "react-redux";
+import { persistor, store } from "./store/store.js";
+import { PersistGate } from "redux-persist/integration/react";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-  <RouterProvider router={router}>
-    <App />
-  </RouterProvider>,
+    <PersistGate loading={null} persistor={persistor}>
+      <RouterProvider router={router}>
+        <App />
+      </RouterProvider>
+    </PersistGate>
   </Provider>
-)
+);
--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,27 @@
 import { createStore } from "redux";
 import { bankReducer } from "./reducers/bankReducers";
+import storage from 'redux-persist/lib/storage'
+import { persistReducer, persistStore } from "redux-persist";
 
-export const store = createStore(bankReducer)
+const persistConfig = {
+    key: 'root',
+    storage,
+  }
+
+
+  const persistedReducer = persistReducer(persistConfig, bankReducer)
+ 
+
+export let store = createStore(persistedReducer)
+export  let persistor = persistStore(store)
+
+
+
+
+
+
+
+// export const store = createStore(bankReducer)
 
 
 console.log(store.getState())
